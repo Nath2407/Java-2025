076a02b684134a0a4266c18181fe77d390423aab
Natali Meza <92613905+Nath2407@users.noreply.github.com>
2025-05-16 01:29:27 -0600

    Create Entrada.java
    Codigo de la clase entrada

---
+++ Sesion1/Reto2/Entrada.java
@@ -0,0 +1,15 @@
+public class Entrada {
+    String nombreEvento;
+    double precio;
+
+    // Constructor
+    public Entrada(String nombreEvento, double precio) {
+        this.nombreEvento = nombreEvento;
+        this.precio = precio;
+    }
+
+    // Método para mostrar información
+    public void mostrarInformacion() {
+        System.out.println("Evento: " + nombreEvento + " | Precio: $" + precio);
+    }
+}
